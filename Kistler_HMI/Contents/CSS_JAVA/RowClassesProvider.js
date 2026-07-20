94bc95bd237530c24453f151782b13b64aa4ae02
Renan Costa <renan.costa@tegan.ie>
2026-07-20 11:23:23 +0100

    Implement alarms, events and fault handling

--- Kistler_HMI/Contents/CSS_JAVA/RowClassesProvider.js
+++ Kistler_HMI/Contents/CSS_JAVA/RowClassesProvider.js
@@ -3,30 +3,103 @@
 
 (function (/** @type {globalThis.TcHmi} */ TcHmi) {
     var Functions;
+
     (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
         var TcHmiProject1;
-        (function (TcHmiProject1) {
-            function RowClassesProvider(RowData, RowIndex, RowNumber) {
-                var cssStyles = []; // Collected CSS styles which will be returned at the end
 
-                    
-                if (RowData.sType == 1) {
-                    cssStyles.push("Alarm");
+        (function (TcHmiProject1) {
+
+            function RowClassesProvider(RowData, RowIndex, RowNumber) {
+                var cssStyles = [];
+
+                if (!RowData) {
+                    return cssStyles;
                 }
-                if (RowData.sType == 2) {
-                    cssStyles.push("Alarm");
+
+                var rawType = RowData.sType;
+
+                /*
+                 * Normalizes both possible formats:
+                 *
+                 * Numeric:
+                 * 1, 2, 3, 4
+                 *
+                 * Text:
+                 * Fault
+                 * Cycle Stop Fault
+                 * Cycle Stop Request
+                 * Message
+                 */
+                var typeNumber;
+
+                if (typeof rawType === "number") {
+                    typeNumber = rawType;
+                } else {
+                    var normalizedType = String(rawType || "")
+                        .trim()
+                        .toLowerCase();
+
+                    switch (normalizedType) {
+                        case "1":
+                        case "fault":
+                        case "alarm":
+                            typeNumber = 1;
+                            break;
+
+                        case "2":
+                        case "cycle stop fault":
+                            typeNumber = 2;
+                            break;
+
+                        case "3":
+                        case "cycle stop request":
+                        case "alert":
+                            typeNumber = 3;
+                            break;
+
+                        case "4":
+                        case "message":
+                            typeNumber = 4;
+                            break;
+
+                        default:
+                            typeNumber = 0;
+                            break;
+                    }
                 }
-                if (RowData.sType == 3) {
-                    cssStyles.push("Alert");
-                }
-                if (RowData.sType == 4) {
-                    cssStyles.push("Message");
+
+                switch (typeNumber) {
+                    case 1:
+                    case 2:
+                        cssStyles.push("Alarm");
+                        break;
+
+                    case 3:
+                        cssStyles.push("Alert");
+                        break;
+
+                    case 4:
+                        cssStyles.push("Message");
+                        break;
                 }
 
                 return cssStyles;
             }
+
             TcHmiProject1.RowClassesProvider = RowClassesProvider;
-        })(TcHmiProject1 = Functions.TcHmiProject1 || (Functions.TcHmiProject1 = {}));
-    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+
+        })(TcHmiProject1 =
+            Functions.TcHmiProject1 ||
+            (Functions.TcHmiProject1 = {}));
+
+    })(Functions =
+        TcHmi.Functions ||
+        (TcHmi.Functions = {}));
+
 })(TcHmi);
-TcHmi.Functions.registerFunctionEx('RowClassesProvider', 'TcHmi.Functions.TcHmiProject1', TcHmi.Functions.TcHmiProject1.RowClassesProvider);
+
+TcHmi.Functions.registerFunctionEx(
+    "RowClassesProvider",
+    "TcHmi.Functions.TcHmiProject1",
+    TcHmi.Functions.TcHmiProject1.RowClassesProvider
+);
